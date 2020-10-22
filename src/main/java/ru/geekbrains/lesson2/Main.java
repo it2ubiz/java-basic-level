242dbe51096795248ee7a3ec129c41488cedc8f6
Nika <zurbaevi.nika@gmail.com>
2020-10-22 16:53:47 +0400

    Fixed minor bugs

--- src/main/java/ru/geekbrains/lesson2/Main.java
+++ src/main/java/ru/geekbrains/lesson2/Main.java
@@ -6,7 +6,7 @@ public class Main {
         /* 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
         С помощью цикла и условия заменить 0 на 1, 1 на 0; */
 
-        invertArray();//
+        invertArray();
 
         ///////////////////////////////////////////////////////////////////////////////////
 
@@ -60,11 +60,7 @@ public class Main {
     public static void invertArray() {
         int[] arr = {1, 0, 1, 0, 0, 1};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
-            }
+            arr[i] = arr[i] == 0 ? 1 : 0;
         }
         for (int value : arr) {
             System.out.print(value + " ");
@@ -74,8 +70,8 @@ public class Main {
 
     public static void fillArray() {
         int[] arr = new int[8];
-        for (int i = 0, j = 0; i < arr.length; i++, j += 3) {
-            arr[i] += j;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
         }
         for (int value : arr) {
             System.out.print(value + " ");
@@ -99,23 +95,22 @@ public class Main {
     public static void fillDiagonal() {
         int[][] arr = new int[5][5];
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0, h = arr[i].length; j < arr[i].length; j++, h--) {
-                if (i == j || i == (h - 1)) {
-                    arr[i][j] = 1;
-                }
-                System.out.print(arr[i][j] + " ");
+            arr[i][i] = 1;
+            arr[i][arr.length - i - 1] = 1;
+        }
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
-            System.out.print("\r\n");
+            System.out.println();
         }
         System.out.println();
     }
 
     public static void searchMaxMin() {
         int[] arr = {1, 5, 8, 9, 21, 2, 6, 19, 4, 55};
-
         int max = arr[0];
         int min = arr[0];
-
         for (int j : arr) {
             if (j > max) {
                 max = j;
@@ -123,25 +118,20 @@ public class Main {
                 min = j;
             }
         }
-
         System.out.println("Max: " + max + " Min: " + min + "\n");
     }
 
     public static boolean checkBalance(int[] arrParam) {
         int lSum, rSum;
-
         for (int i = 0; i < arrParam.length + 1; i++) {
             lSum = 0;
             rSum = 0;
-
             for (int j = 0; j < i; j++) {
                 lSum += arrParam[j];
             }
-
             for (int j = i; j < arrParam.length; j++) {
                 rSum += arrParam[j];
             }
-
             if (lSum == rSum) return true;
         }
         return false;
@@ -153,23 +143,22 @@ public class Main {
                 int buf = arrParam[arrParam.length - 1];
                 System.arraycopy(arrParam, 0, arrParam, 1, arrParam.length - 1);
                 arrParam[0] = buf;
-
-                for (int i : arrParam) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
+                printArray(arrParam);
             }
         } else if (n < 0) {
             for (int x = 0; x > n; x--) {
                 int buf = arrParam[0];
                 System.arraycopy(arrParam, 1, arrParam, 0, arrParam.length - 1);
                 arrParam[arrParam.length - 1] = buf;
-
-                for (int i : arrParam) {
-                    System.out.print(i + " ");
-                }
-                System.out.print("\r\n");
+                printArray(arrParam);
             }
+        }
+        System.out.println();
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
         }
     }
 }
